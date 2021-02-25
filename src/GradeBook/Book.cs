b20ba79f356f4d6350da6272e72d6e60b60dc5ad
Jake Chambers <jake.chambers@convergencegroup.co.uk>
2021-02-25 21:40:08 +0000

    FInal commit 25/02/21

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,22 +1,43 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Book
     {
-        public Book()
+        public Book(string name)
         {
             grades = new List<double>();
+            this.name = name;
         }
         public void AddGrade(double grade)
         {
+            grades.Add(grade);
+        }
 
-            if (grade >= 0 && grade <= 100)
+        public void ShowStatistics()
+        {
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var number in grades)
             {
-                grades.Add(grade);
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
             }
 
-           private List<double> grades;
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The lowest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+
+        static private List<double> grades;
+        private string name;
     }
-}
 }
