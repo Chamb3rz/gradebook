c7589990caf2644841640490aff4ea83ced5c877
Jake Chambers <jake.chambers@convergencegroup.co.uk>
2021-03-07 20:58:34 +0000

    unit tests 07/03

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,6 +5,83 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
+
+        [Fact]
+        public void NewTest()
+        {
+            var x = GetInt();
+            SetInt(x);
+
+            Assert.Equal(x, 3);
+        }
+
+        private void SetInt(int x)
+        {
+            x = 43;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+
+            Assert.Equal(book1.Name, "New Name");
+        }
+
+        private void GetBookSetName(ref Book book, string name)
+        {
+           book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal(book1.Name, "Book 1");
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+           book = new Book(name);
+        }
+
+        [Fact]
+        public void canSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal(book1.Name, "New Name");
+        }
+
+        private void SetName(Book book, string name)
+        {
+           book.Name = name; 
+        }
+
+        [Fact]
+        public void StringBehaveLikeValueType()
+        {
+            string name = "Jake";
+            string upper = MakeUpper(name);
+
+            Assert.Equal(name, "Jake");
+            Assert.Equal(upper, "JAKE");
+        }
+
+        private string MakeUpper(string name)
+        {
+            return name.ToUpper();
+        }
+
         [Fact]
         public void GetBookReturnsDifferentObjects()
         {
