9e373bb6bd3839671d8c2220348f24280aa0ad70
Jake Chambers <jake.chambers@convergencegroup.co.uk>
2021-03-09 22:09:04 +0000

    unit tests, exception and loops 09/03/21

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -10,9 +10,38 @@ namespace GradeBook
             grades = new List<double>();
             Name = name;
         }
+
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+
+            }
+        }
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
         }
 
          public Statistics GetStatistics()
@@ -23,14 +52,37 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach (var grade in grades)
+            for(var i = 0; i < grades.Count; i++ )
             { 
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
+                result.Low = Math.Min(grades[i], result.Low);
+                result.High = Math.Max(grades[i], result.High);
+                result.Average += grades[i];
             }
-
+ 
             result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                
+                 case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                 case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                 case var d when d >= 60.0:
+                    result.Letter = 'C';
+                    break;
+                
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
